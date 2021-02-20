8c6c0b35503176ebeb42c92d1d392e65f18f049e
Alex Dodd <alexdodd@gmail.com>
2021-02-20 21:58:38 +0000

    created an Accessory class

---
+++ src/main/java/Accessory.java
@@ -0,0 +1,8 @@
+public abstract class Accessory extends StockItem implements ISell{
+
+    public Accessory(String name, double retailPrice, double costPrice) {
+        super(name, retailPrice, costPrice);
+    }
+
+    public abstract double calculateMarkUp();
+}
