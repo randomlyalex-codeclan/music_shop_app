1b8595b2786ee13efcc38118532751f83370be3d
Alex Dodd <alexdodd@gmail.com>
2021-02-20 21:12:15 +0000

    abstract Instrument Class created

---
+++ src/main/java/Instrument.java
@@ -0,0 +1,12 @@
+public abstract class Instrument extends StockItem {
+    private String colour;
+    private String material;
+    private String family;
+
+    public Instrument(String name, double retailPrice, double costPrice, String colour, String material, String family) {
+        super(name, retailPrice, costPrice);
+        this.colour = colour;
+        this.material = material;
+        this.family = family;
+    }
+}
